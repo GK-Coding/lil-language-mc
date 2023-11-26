f01dc31de35d5d51aa3761ab4283495fdf9b5f4b
Giovanni Kane <gio.kane1124@gmail.com>
2023-11-25 21:43:43 -0500

    Update assessScore.ts

--- components/assessScore.ts
+++ components/assessScore.ts
@@ -4,7 +4,7 @@ import { getRhymes } from "@/app/server";
 
 export const assessScore = async (lines: string[], word: string) => {
     const endWords = lines.filter(line => {
-        return line.split(" ").length > 3;
+        return line.split(" ").length >= 3;
     }).map(line2 => {
         const wordsInLine = line2.split(" ");
         return wordsInLine[wordsInLine.length - 1];
